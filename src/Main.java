efb580c5ef4c211746a33bf221450492125d809d
Fevenf <101144416+Fevenf@users.noreply.github.com>
2022-05-17 20:09:09 -0400

    First commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+        TripleTacoBox tacoBox1 = new TripleTacoBox();
+        tacoBox1.eat();
+        System.out.println(tacoBox1.tacosRemaining());
+    }
+}
